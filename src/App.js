0cd873e459c7a5fb1bb3752d616222f57da7e884
Ryan Guide <ryan.guide@comcast.net>
2020-07-25 17:16:55 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ThreeByThree from "./Matrix";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      a: "",
+      b: "",
+      c: "",
+      d: "",
+      det: "",
+      e:"",
+      f:"",
+      g:"",
+      h:"",
+      allowInvert: false,
+      showDet: false
+    }
+  }
+  render() {
+    return(
+      <div className="body">
+        <h4>Matrix Inverter</h4>
+        <div className="vector">
+        <table className="matrix">
+              <tr>
+                <td><input onChange={this.handleChange} name="a" placeholder="a" value={this.state.a}></input></td>
+                <td><input onChange={this.handleChange} name="b" placeholder="b" value={this.state.b}></input></td>
+              </tr>
+              <tr>
+                <td><input onChange={this.handleChange} name="c" placeholder="c" value={this.state.c}></input></td>
+                <td><input onChange={this.handleChange} name="d" placeholder="d" value={this.state.d}></input></td>
+              </tr>
+            </table>
+            </div>
+            <br />
+          <button onClick={this.invert}>Invert</button>
+          <button onClick={this.reset}>Reset</button>
+          <hr />
+          <div>
+            {this.state.showDet? <p>det = {this.state.det}</p> : <p></p>}
+            <br />
+            {this.state.allowInvert? 
+            <div className="vector">
+              <table className="matrix">
+                <tr>
+                  <td>{this.state.h}</td>
+                  <td>{this.state.f}</td>
+                </tr>
+                <tr>
+                  <td>{this.state.g}</td>
+                  <td>{this.state.e}</td>
+                </tr>
+              </table>
+            </div> : <p>If determinant is equal to 0, the matrix is not invertible.</p>}
+          </div>
+          <hr />
+              <ThreeByThree />
+      </div>
+    )
+  }
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+  invert = () => {
+    this.setState(state => ({
+      det: state.a * state.d - state.b * state.c,
+      showDet: true
+    }));
+    if (this.state.det === 0) {
+      this.setState({
+        allowInvert: false
+      })
+    } else {
+      this.setState(state => ({
+        allowInvert: true,
+        e: state.a/state.det,
+        f: state.b/state.det*-1,
+        g: state.c/state.det*-1,
+        h: state.d/state.det,
+      }))
+    }
+  }
+  reset = () => {
+    this.setState(state => ({
+      a:"",
+      b:"",
+      c:"",
+      d:"",
+      det:"",
+      showDet:"",
+      allowInvert: false
+    }))
+  }
 }
 
 export default App;
---
+++ src/Matrix.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+class ThreeByThree extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            a11: "",
+            a12: "",
+            a13: "",
+            a21: "",
+            a22: "",
+            a23: "",
+            a31: "",
+            a32: "",
+            a33: "",
+            b11: "",
+            b12: "",
+            b13: "",
+            b21: "",
+            b22: "",
+            b23: "",
+            b31: "",
+            b32: "",
+            b33: "",
+            det: ""
+        }
+    }
+    render(){
+        return(
+            <div>
+                <h5>3x3 Matrix Inverter</h5>
+                <div className="vector">
+                <table className="matrix">
+                  <tr>
+                    <td><input onChange={this.handleChange} name="a11" placeholder="a11" value={this.state.a11}></input></td>
+                    <td><input onChange={this.handleChange} name="a12" placeholder="a12" value={this.state.a12}></input></td>
+                    <td><input onChange={this.handleChange} name="a13" placeholder="a13" value={this.state.a13}></input></td>
+                  </tr>
+                  <tr>
+                    <td><input onChange={this.handleChange} name="a21" placeholder="a21" value={this.state.a21}></input></td>
+                    <td><input onChange={this.handleChange} name="a22" placeholder="a22" value={this.state.a22}></input></td>
+                    <td><input onChange={this.handleChange} name="a23" placeholder="a23" value={this.state.a23}></input></td>
+                  </tr>
+                  <tr>
+                    <td><input onChange={this.handleChange} name="a31" placeholder="a31" value={this.state.a31}></input></td>
+                    <td><input onChange={this.handleChange} name="a32" placeholder="a32" value={this.state.a32}></input></td>
+                    <td><input onChange={this.handleChange} name="a33" placeholder="a33" value={this.state.a33}></input></td>
+                  </tr>
+                </table>
+                </div>
+            </div>
+        )
+    }
+    handleChange = (event) => {
+        this.setState({
+          [event.target.name]: event.target.value
+        });
+      }
+    calculateDet = () => {
+        this.setState(state => ({
+
+        }))
+    }
+}
+
+export default ThreeByThree;
